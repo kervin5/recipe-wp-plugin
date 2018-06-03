5388f86b6dda704b5ce4aa313101fb97397f9a2d
Kervin Vasquez <kervinlvh@gmail.com>
2018-06-03 16:55:24 -0700

    Implement custom metadata fields for recipe

--- includes/admin/recipe-options.php
+++ includes/admin/recipe-options.php
@@ -1,5 +1,40 @@
-<?php
+<?php function r_recipe_options_mb($post){
+	$recipe_data = get_post_meta($post->ID,'recipe_data',true);
 
-function r_recipe_options_mb(){
-    echo "Hello World";
+	if(empty($recipe_data)){
+		$recipe_data = array(
+			"ingredients" => "",
+			"time"=> "",
+			"utensils" => "",
+			"level" => "Beginner",
+			"meal_type" => ""
+		);
+	}
+	?>
+
+	<div class="form-group">
+		<label>Ingredients</label>
+		<input type="text" class="form-control" name="r_inputIngredients" value="<?php echo $recipe_data['ingredients']?>">
+	</div>
+	<div class="form-group">
+		<label>Cooking Time Required</label>
+		<input type="text" class="form-control" name="r_inputTime" value="<?php echo $recipe_data['time']?>">
+	</div>
+	<div class="form-group">
+		<label>Utensils</label>
+		<input type="text" class="form-control" name="r_inputUtensils" value="<?php echo $recipe_data['utensils']?>">
+	</div>
+	<div class="form-group">
+		<label>Cooking Experience</label>
+		<select class="form-control" name="r_inputLevel">
+			<option value="Beginner">Beginner</option>
+			<option value="Intermediate" <?php echo $recipe_data['level']== "Intermediate" ? "SELECTED" : "" ?> >Intermediate</option>
+			<option value="Expert" <?php echo $recipe_data['level']== "Expert" ? "SELECTED" : "" ?>>Expert</option>
+		</select>
+	</div>
+	<div class="form-group">
+		<label>Meal Type</label>
+		<input type="text" class="form-control" name="r_inputMealType" value="<?php echo $recipe_data['meal_type']?>" >
+	</div>
+<?php
 }
