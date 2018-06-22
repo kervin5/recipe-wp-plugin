e22d8af992f77e089c0b02353aa061e062ebb689
Kervin Vasquez <kervinlvh@gmail.com>
2018-06-21 21:49:58 -0700

    Implement frontend recipe submission

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -12,4 +12,34 @@ jQuery(function($){
             console.log(data);
         });
     });
+
+    $("#recipe-form").on("submit",function (e) {
+        e.preventDefault();
+
+        $(this).hide();
+
+        $("#recipe-status").html('<div class="alert alert-info text-center">Please wait!</div> ');
+
+        var form = {
+            action: "r_submit_user_recipe",
+            content: tinymce.activeEditor.getContent(),
+            title: $("#r_inputTitle").val(),
+            ingredients: $("#r_inputTitle").val(),
+            time: $("#r_inputTime").val(),
+            utensils: $("#r_inputUtensils").val(),
+            level: $("#r_inputLevel").val(),
+            meal_type: $("#r_inputMealType").val()
+        };
+
+        console.log(form);
+
+        $.post(recipe_obj.ajax_url,form).always(function (response) {
+            if(response.status == 2) {
+                $("#recipe-status").html('<div class="alert alert-success text-center">Recipe submitted successfully!</div> ');
+            }else {
+                $("#recipe-status").html('<div class="alert alert-danger text-center">Unable to submit recipe. Please fill in all fields.</div> ');
+                $("#recipe-form").show();
+            }
+        })
+    });
 });
