e37f4e60432fed090b8cf323473e803c528ec758
Kervin Vasquez <kervinlvh@gmail.com>
2018-06-28 20:00:52 -0700

    Add custom option for recipe plugin

--- includes/activate.php
+++ includes/activate.php
@@ -29,4 +29,14 @@ function r_activate_plugin(){
 	    'daily',
 	    'r_daily_recipe_hook'
     );
+
+    $recipe_opts = get_option('r_opts');
+
+    if(!$recipe_opts){
+    	$opts = [
+    		'rating_login_required' =>1,
+		    "recipe_submission_login_required" => 1 ];
+
+    	add_option('r_opts',$opts);
+    }
 }
